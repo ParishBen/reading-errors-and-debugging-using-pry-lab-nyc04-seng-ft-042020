8dd3d625eec3cabc445481c10189da260a9f1598
Ben Parish <parishben13@gmail.com>
2020-04-07 00:14:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -18,9 +18,8 @@ def selection(num)
     puts  "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
      puts "HAM HAM HAM IN MY TUMMY"
-  binding.pry
   end
-
+binding.pry
 def runner
   prompt_user
   num = get_user_input
